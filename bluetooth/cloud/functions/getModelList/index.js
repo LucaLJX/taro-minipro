5a7dbb4c8fea0f190165b541041ecbcb53913915
luca_ljx <liujingxin06127@hellobike.com>
2019-08-17 20:59:01 +0800

    A - 新增获取设备型号云函数：getModelList

---
+++ bluetooth/cloud/functions/getModelList/index.js
@@ -0,0 +1,21 @@
+const cloud = require('wx-server-sdk')
+const _ = require('lodash')
+
+cloud.init()
+const db = cloud.database({
+  env: 'easybluetoothdev-d4pjb'
+})
+const easync_model_list = db.collection('easync_model_list')
+
+exports.main = async (event, content, cb) => {
+  const result = (await easync_model_list.orderBy('sortNum', 'asc').get()).data
+  if (result.length === 0) {
+    return []
+  }
+  _.map(result, (item) => {
+    if (item.children && item.children.length !== 0) {
+      item.children = _.sortBy(item.children, childItem => childItem.sortNum)
+    }
+  })
+  return result
+}

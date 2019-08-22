18a9e8f264e342fad9723805bb5ad5bbdd0a4ee2
luca_ljx <liujingxin06127@hellobike.com>
2019-08-23 02:17:31 +0800

    A - 新增 根据id获取设备列表云函数 getDeviceByIds

---
+++ bluetooth/cloud/functions/getDeviceByIds/index.js
@@ -0,0 +1,37 @@
+const cloud = require('wx-server-sdk')
+const _ = require('lodash')
+
+cloud.init()
+const db = cloud.database({
+  env: 'easybluetoothdev-d4pjb'
+})
+const easync_device = db.collection('easync_device')
+
+exports.main = async (event, content, cb) => {
+  const { deviceIds } = event
+  if (deviceIds.length === 0) {
+    return {
+      code: 0,
+      data: []
+    }
+  }
+  let list = []
+  for (let i = 0; i < deviceIds.length; i++) {
+    const node = deviceIds[i]
+    const res = await easync_device.where({
+      deviceId: node
+    }).get()
+    if (res.errMsg.indexOf('ok') === -1) {
+      return {
+        code: 999,
+        data: null
+      }
+    }
+    list = list.concat(res.data)
+  }
+  const result = _.uniqBy(list, 'deviceId')
+  return {
+    code: 0,
+    data: result
+  }
+}

daeb7fa1b5c5bddf7748cf98a364042b613d45ca
liujingxin666 <liujingxin@bixin.cn>
2020-11-09 22:02:37 +0800

    add：utils

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react'
+import request from '../../utils/request'
 
 const Test = () => {
 
+  const testRequest = async () => {
+    const result = await request.postParams('http://www.baidu.com', { name: 111 })
+  }
+
   useEffect(() => {
     console.log(123)
+    testRequest()
   }, [])
 
   return 123
--- src/utils/request.js
+++ src/utils/request.js
@@ -5,11 +5,39 @@ const defaultConfig = {
   timeout: 3000, // 超时 3000 ms
 }
 
+// code = 200, msg, data
 
-export const get = (url, params, options) => Taro.request({
-  url: url,
-  method: 'GET',
-  header: {
-    'content-type': 'application/json', // default
-  },
-})
+const request = (url, data, options) => {
+  const hideError = options.hideError || true
+
+  return new Promise((resolve, reject) => {
+    Taro.request({
+      url: url,
+      method: options.method,
+      header: {
+        'content-type': options.contentType || 'application/json', // default
+      },
+      data: data,
+      ...defaultConfig,
+      success: data => {
+        console.log('success')
+        console.log(data)
+        resolve(data)
+      },
+      fail: e => {
+        console.log('err')
+        console.log(e)
+        hideError && alert(e)
+        reject(e)
+      }
+    })
+  })
+}
+
+export default {
+  get: (url, data, options = {}) => request(url, data, { ...options, method: 'GET' }),
+  post: (url, data, options = {}) => request(url, data, { ...options, method: 'POST' }),
+  postParams: (url, data, options = {}) => request(url, data, { ...options, method: 'POST' }),
+}
+
+
